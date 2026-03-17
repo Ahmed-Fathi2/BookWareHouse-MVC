9602d2e31e53335c3268c212012bb9617e3558dd
Fathi <ahmedfathi5005@gmail.com>
2026-03-17 04:53:20 +0200

    Initial Create

---
+++ BookWarehouse.Presentation/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookWarehouse.Presentation.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
