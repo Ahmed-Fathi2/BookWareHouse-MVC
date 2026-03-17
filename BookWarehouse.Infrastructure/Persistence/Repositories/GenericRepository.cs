b17907a949bbf24b4746668af3730fe1ea1ba8d8
Fathi <ahmedfathi5005@gmail.com>
2026-03-17 04:52:14 +0200

    Add project files.

---
+++ BookWarehouse.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookWarehouse.Domain.Repositories;
+using BookWarehouse.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookWarehouse.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<TEntity, TKey>(ApplicationDbContext dbContext) : IGenericRepository<TEntity, TKey> where TEntity : class
+    {
+        protected readonly ApplicationDbContext _dbContext = dbContext;
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+          return  await  _dbContext.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(id);  
+        }
+
+    
+        public void Add(TEntity category)
+        {
+            _dbContext.Add(category);
+        }
+
+        public void Delete(TEntity category)
+        {
+            _dbContext.Remove(category);
+        }
+    }
+}
